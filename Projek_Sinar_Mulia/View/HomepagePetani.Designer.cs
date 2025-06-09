6db97c47798a7144c9f6b40f8cb930026f2c0824
ryn-9 <mbinroy@gmail.com>
2025-06-10 00:59:06 +0700

    ihh...

--- Projek_Sinar_Mulia/View/HomepagePetani.Designer.cs
+++ Projek_Sinar_Mulia/View/HomepagePetani.Designer.cs
@@ -39,6 +39,7 @@
             pnlutama = new Panel();
             pnlsidebar = new Panel();
             panel2 = new Panel();
+            btnlihatstatus = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)picindikator).BeginInit();
             pnlsidebar.SuspendLayout();
@@ -48,9 +49,10 @@
             // 
             pictureBox1.BackColor = Color.FromArgb(50, 107, 74);
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(12, 24);
+            pictureBox1.Location = new Point(17, 40);
+            pictureBox1.Margin = new Padding(4, 5, 4, 5);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(33, 33);
+            pictureBox1.Size = new Size(47, 55);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
@@ -61,9 +63,10 @@
             label1.BackColor = Color.FromArgb(50, 107, 74);
             label1.Font = new Font("Microsoft Sans Serif", 9.749999F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.Control;
-            label1.Location = new Point(48, 32);
+            label1.Location = new Point(69, 53);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(130, 16);
+            label1.Size = new Size(186, 25);
             label1.TabIndex = 0;
             label1.Text = "Sinar Mulia Group";
             label1.TextAlign = ContentAlignment.MiddleCenter;
@@ -71,9 +74,10 @@
             // picindikator
             // 
             picindikator.Image = (Image)resources.GetObject("picindikator.Image");
-            picindikator.Location = new Point(0, 62);
+            picindikator.Location = new Point(0, 103);
+            picindikator.Margin = new Padding(4, 5, 4, 5);
             picindikator.Name = "picindikator";
-            picindikator.Size = new Size(11, 45);
+            picindikator.Size = new Size(16, 75);
             picindikator.SizeMode = PictureBoxSizeMode.Zoom;
             picindikator.TabIndex = 5;
             picindikator.TabStop = false;
@@ -87,11 +91,11 @@
             btnlahan.Font = new Font("Microsoft Sans Serif", 11.999999F);
             btnlahan.ForeColor = Color.FromArgb(50, 107, 74);
             btnlahan.Image = (Image)resources.GetObject("btnlahan.Image");
-            btnlahan.Location = new Point(-20, 159);
+            btnlahan.Location = new Point(-29, 265);
             btnlahan.Margin = new Padding(0);
             btnlahan.Name = "btnlahan";
-            btnlahan.Padding = new Padding(10, 0, 10, 0);
-            btnlahan.Size = new Size(261, 39);
+            btnlahan.Padding = new Padding(14, 0, 14, 0);
+            btnlahan.Size = new Size(373, 65);
             btnlahan.TabIndex = 4;
             btnlahan.Text = "Lahan";
             btnlahan.TextImageRelation = TextImageRelation.ImageBeforeText;
@@ -105,11 +109,11 @@
             btnriwayat.Font = new Font("Microsoft Sans Serif", 11.999999F);
             btnriwayat.ForeColor = Color.FromArgb(50, 107, 74);
             btnriwayat.Image = (Image)resources.GetObject("btnriwayat.Image");
-            btnriwayat.Location = new Point(-11, 112);
+            btnriwayat.Location = new Point(-16, 187);
             btnriwayat.Margin = new Padding(0);
             btnriwayat.Name = "btnriwayat";
-            btnriwayat.Padding = new Padding(10, 0, 10, 0);
-            btnriwayat.Size = new Size(209, 39);
+            btnriwayat.Padding = new Padding(14, 0, 14, 0);
+            btnriwayat.Size = new Size(299, 65);
             btnriwayat.TabIndex = 3;
             btnriwayat.Text = "Lihat Riwayat";
             btnriwayat.TextAlign = ContentAlignment.MiddleRight;
@@ -124,11 +128,11 @@
             btnkelolapermintaan.Font = new Font("Microsoft Sans Serif", 11.249999F, FontStyle.Regular, GraphicsUnit.Point, 0);
             btnkelolapermintaan.ForeColor = Color.FromArgb(50, 107, 74);
             btnkelolapermintaan.Image = (Image)resources.GetObject("btnkelolapermintaan.Image");
-            btnkelolapermintaan.Location = new Point(0, 65);
+            btnkelolapermintaan.Location = new Point(0, 108);
             btnkelolapermintaan.Margin = new Padding(0);
             btnkelolapermintaan.Name = "btnkelolapermintaan";
-            btnkelolapermintaan.Padding = new Padding(10, 0, 10, 0);
-            btnkelolapermintaan.Size = new Size(212, 39);
+            btnkelolapermintaan.Padding = new Padding(14, 0, 14, 0);
+            btnkelolapermintaan.Size = new Size(303, 65);
             btnkelolapermintaan.TabIndex = 2;
             btnkelolapermintaan.Text = "Ajukan Permintaan";
             btnkelolapermintaan.TextAlign = ContentAlignment.MiddleRight;
@@ -139,21 +143,24 @@
             // panel1
             // 
             panel1.BackColor = Color.Gainsboro;
-            panel1.Location = new Point(-41, 0);
+            panel1.Location = new Point(-59, 0);
+            panel1.Margin = new Padding(4, 5, 4, 5);
             panel1.Name = "panel1";
-            panel1.Size = new Size(882, 20);
+            panel1.Size = new Size(1260, 33);
             panel1.TabIndex = 4;
             // 
             // pnlutama
             // 
-            pnlutama.Location = new Point(201, 20);
+            pnlutama.Location = new Point(287, 33);
+            pnlutama.Margin = new Padding(4, 5, 4, 5);
             pnlutama.Name = "pnlutama";
-            pnlutama.Size = new Size(601, 429);
+            pnlutama.Size = new Size(859, 715);
             pnlutama.TabIndex = 5;
             // 
             // pnlsidebar
             // 
             pnlsidebar.BackColor = Color.LightGreen;
+            pnlsidebar.Controls.Add(btnlihatstatus);
             pnlsidebar.Controls.Add(picindikator);
             pnlsidebar.Controls.Add(pictureBox1);
             pnlsidebar.Controls.Add(label1);
@@ -163,26 +170,46 @@
             pnlsidebar.Controls.Add(btnkelolapermintaan);
             pnlsidebar.Dock = DockStyle.Left;
             pnlsidebar.Location = new Point(0, 0);
+            pnlsidebar.Margin = new Padding(4, 5, 4, 5);
             pnlsidebar.Name = "pnlsidebar";
-            pnlsidebar.Size = new Size(198, 449);
+            pnlsidebar.Size = new Size(283, 748);
             pnlsidebar.TabIndex = 3;
             // 
             // panel2
             // 
             panel2.BackColor = Color.FromArgb(50, 107, 74);
-            panel2.Location = new Point(0, 20);
+            panel2.Location = new Point(0, 33);
+            panel2.Margin = new Padding(4, 5, 4, 5);
             panel2.Name = "panel2";
-            panel2.Size = new Size(200, 42);
+            panel2.Size = new Size(286, 70);
             panel2.TabIndex = 6;
+            // 
+            // btnlihatstatus
+            // 
+            btnlihatstatus.FlatAppearance.BorderSize = 0;
+            btnlihatstatus.FlatStyle = FlatStyle.Flat;
+            btnlihatstatus.Font = new Font("Microsoft Sans Serif", 11.999999F);
+            btnlihatstatus.ForeColor = Color.FromArgb(50, 107, 74);
+            btnlihatstatus.Image = (Image)resources.GetObject("btnlihatstatus.Image");
+            btnlihatstatus.Location = new Point(-14, 330);
+            btnlihatstatus.Margin = new Padding(0);
+            btnlihatstatus.Name = "btnlihatstatus";
+            btnlihatstatus.Padding = new Padding(14, 0, 14, 0);
+            btnlihatstatus.Size = new Size(373, 65);
+            btnlihatstatus.TabIndex = 7;
+            btnlihatstatus.Text = "Lihat Status";
+            btnlihatstatus.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnlihatstatus.UseVisualStyleBackColor = true;
             // 
             // HomepagePetani
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 449);
+            ClientSize = new Size(1143, 748);
             Controls.Add(panel1);
             Controls.Add(pnlsidebar);
             Controls.Add(pnlutama);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "HomepagePetani";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Homepage Petani";
@@ -205,5 +232,6 @@
         private Panel pnlutama;
         private Panel pnlsidebar;
         private Panel panel2;
+        private Button btnlihatstatus;
     }
 }
